8f2cba10fa30def38f2454efc6e5c3f03109377b
Neptune <neptunedays9@gmail.com>
2020-04-07 19:31:07 +1000

    Modified devices display

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,53 @@
 
 import * as React from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Button } from 'react-native';
+import DeviceTile from './DeviceTile'
 
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+  
 export default class Dashboard extends React.Component {
+
+
     render() {
         return (
-            <View>
-                <Text>Dashboard screen</Text>
-            </View>
+        <View style={{ flex: 1, margin: 5, backgroundColor: '#ddd', height: 130}}>
+             <FlatList
+                data={DATA}
+                renderItem={({ item }) => <DeviceTile props={item}/>}
+                keyExtractor={item => item.id}
+                numColumns={1}
+            />
+        </View>
         );
     }
 }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      //marginTop: Constants.statusBarHeight,
+    },
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  })
+  
---
+++ src/components/DeviceTile.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Button} from 'react-native';
+
+export default class DeviceTile extends React.Component {
+
+    
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
+    
+
+    render() {
+        return(
+            <Button title={"test"}/>
+        );
+    }
+}
